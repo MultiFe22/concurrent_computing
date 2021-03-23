541ae9e7805f900da110db2fdb20cdd999d3e996
Felipe Vidal <felipevcb@dcc.ufrj.br>
2021-03-23 20:52:05 -0300

    second commit

--- first.c
+++ first.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
 #include<pthread.h>
 
+#define NTHREADS 10
+
 void * job (void * arg){
-	printf("Hi, this is the new thread!\n");
+	int ident = * (int *) arg;
+	printf("Hi, this is the new thread %d!\n", ident);
 	pthread_exit(NULL);
 }
 
 int main (void){
-	pthread_t tid; //thread identifier
-	//create a new thread
-	if (pthread_create(&tid,NULL, job, NULL)){ 
-		printf("ERROR -- pthread_create\n");
-	}
+	pthread_t tid[NTHREADS]; //thread identifier
+	int ident[NTHREADS]; //local identifier
+	//create new threadS
+	for(int i=1; i<=NTHREADS; ++i){
+		ident[i-1] = i;
+		if (pthread_create(&tid[i-1], NULL, job, (void *)&ident[i-1])){ 
+			printf("ERROR -- pthread_create\n");
+		}
+	}	
 	//print welcome
 	printf("Hi, this is the main thread!\n");
 	//separates main
